ac401264d3c7c63452a394b777a678b11f588f53
EhWanu <cameronjshadgett@gmail.com>
2021-12-08 11:32:38 -0500

    post new User function

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
-	.connect(process.env.MONGO_URL)
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
 	.then(console.log("Connected the goose"))
 	.catch((err) => console.error(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
 	console.log("Backend is running!");
